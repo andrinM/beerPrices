6494895de68c7277a503b37db158d2f41185b5c8
andrinM <andrin@14us.ch>
2026-01-20 21:41:15 +0100

    [IMP] Added InfoDropdown

--- my-web-app/frontend/src/components/Layout/InfoDropDown.jsx
+++ my-web-app/frontend/src/components/Layout/InfoDropDown.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Button } from 'react-bootstrap'
+import { Container, Button, Collapse } from 'react-bootstrap'
 import DropdownIcon from '../icons/DropdownIcon'
 
 const InfoDropDown = ({ title, text }) => {
@@ -10,17 +10,24 @@ const InfoDropDown = ({ title, text }) => {
     };
 
     return (
-        <Container>
-            <div className="info-box-title mt-5">
-                <div className="d-flex align-items-center justify-content-between">
+        <Container id="info-container" className="mb-5">
+            <div id="info-box-title">
+                <div className="d-flex align-items-center justify-content-between px-md-5 py-md-5 px-4 py-2 ">
                     <h3>{title}</h3>
                     <Button onClick={toggleDropdown} className={`btn-icon-only ${dropdownOpen ? "dropdown-open" : "dropdown-closed"}`} variant="none">
                         <DropdownIcon />
                     </Button>
                 </div>
             </div>
-            <span>{text}</span>
-
+            <Collapse in={dropdownOpen}>
+                <div id="info-collapse-text">
+                    <div className="px-md-5 py-md-5 px-4 py-2">
+                        <div className="mt-3 text-muted">
+                            {text}
+                        </div>
+                    </div>
+                </div>
+            </Collapse>
         </Container>
     )
 
--- my-web-app/frontend/src/pages/Info.jsx
+++ my-web-app/frontend/src/pages/Info.jsx
@@ -1,7 +1,10 @@
 import InfoDropDown from '../components/Layout/InfoDropDown';
 function Info() {
     return (
-        <InfoDropDown title="Wichtige Infos" text="Wichtiger text den es zu bearbeiten gilt" />
+        <>
+            <InfoDropDown title="Wichtige Infos" text="Wichtiger text den es zu bearbeiten gilt" />
+            <InfoDropDown title="Tickets" text="Wichtiger text den es zu bearbeiten gilt" />
+        </>
     )
 };
 
